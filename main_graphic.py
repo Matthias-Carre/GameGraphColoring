cf678a189c822e5d04b3de460f971b0fea9d0103
Matthias-Carre <lemattheuxbis@gmail.com>
2025-10-17 09:45:40 +0200

    ajout commentaires

--- main_graphic.py
+++ main_graphic.py
@@ -2,16 +2,14 @@ import tkinter as tk
 from Grid import Grid
 
 colors = ["Red", "Green", "Blue", "purple"]
-color_selected = -1
+color_selected = -1 # given by the radio button
 
 def on_button_click(event):
-    #we get the name of the button clicked
     x = event.x
     y = event.y
     ratio = min(800 / grid.width, 800 / grid.height)
     i = int(x // ratio)
     j = int(y // ratio)
-    print(f'clic: {x}, {y} -> {i}, {j}')
     
     if (0 <= i) and (i < grid.width) and (0 <= j) and (j < grid.height) and (color_selected.get() != -1):
         print(f'Button clicked at: {i}, {j}') 
@@ -23,7 +21,7 @@ def on_button_click(event):
 def draw_rectangle(x, y, width, height, color):#draw rectangle form x,y to x+width,y+height
     canvas.create_rectangle(x, y, x + width, y + height, fill=color, outline="")
 
-def draw_circle(x, y, radius, color):
+def draw_circle(x, y, radius, color):#draw circle centered in x,y with radius
     canvas.create_oval(x - radius, y - radius, x + radius, y + radius, fill=color, outline="")
     canvas.create_oval(x - radius/1.5, y - radius/1.5, x + radius/1.5, y + radius/1.5, fill="white", outline="")
 
@@ -31,7 +29,7 @@ def change_node_color(grid, x, y, color):
     grid.set_cell(x, y, color)
 
 
-def draw_grid(grid):
+def draw_grid(grid):#draw the grid with circles of colors of the nodes
     ratio = min(800 / grid.width, 800 / grid.height)
 
     for i in range(grid.width):
@@ -51,14 +49,17 @@ if __name__=="__main__":
 
     grid = Grid(10, 5)
 
+
     root = tk.Tk()
     root.title("Graph Coloring Game")
     
+    #set the window size to match the gird of the graph
     ratio = min(800 / grid.width, 800 / grid.height)
     w = ratio * grid.width
     h = ratio * grid.height
     root.geometry(f'{int(w)}x{int(h)+100}')
     canvas = tk.Canvas(root, width=w, height=h, bg="white")
+
     canvas.bind("<Button-1>", on_button_click)
     color_selected = tk.IntVar(value=-1)  
     
