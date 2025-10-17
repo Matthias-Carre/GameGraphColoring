82f6c1fe7498b4f517648c49db87b4361c5507c1
Matthias Carré <lemattheuxbis@gmail.com>
2025-10-17 10:13:14 +0200

    undo button

--- main_graphic.py
+++ main_graphic.py
@@ -25,6 +25,10 @@ def draw_circle(x, y, radius, color):#draw circle centered in x,y with radius
     canvas.create_oval(x - radius, y - radius, x + radius, y + radius, fill=color, outline="")
     canvas.create_oval(x - radius/1.5, y - radius/1.5, x + radius/1.5, y + radius/1.5, fill="white", outline="")
 
+def undo_last_move():
+    grid.undo_last_move()
+    draw_grid(grid)
+
 def change_node_color(grid, x, y, color):
     grid.set_cell(x, y, color)
 
@@ -42,12 +46,12 @@ def draw_grid(grid):#draw the grid with circles of colors of the nodes
 def draw_color_selection():
     for i, color in enumerate(colors):
         tk.Radiobutton(root, text=color, value=i,variable=color_selected, fg=color).pack(anchor="w")
-
+    tk.Button(root, text="Undo Last Move", command=undo_last_move).pack()
 
 
 if __name__=="__main__":
 
-    grid = Grid(10, 5)
+    grid = Grid(5, 5)
 
 
     root = tk.Tk()
@@ -57,7 +61,7 @@ if __name__=="__main__":
     ratio = min(800 / grid.width, 800 / grid.height)
     w = ratio * grid.width
     h = ratio * grid.height
-    root.geometry(f'{int(w)}x{int(h)+100}')
+    root.geometry(f'{int(w)}x{int(h)+150}')
     canvas = tk.Canvas(root, width=w, height=h, bg="white")
 
     canvas.bind("<Button-1>", on_button_click)
